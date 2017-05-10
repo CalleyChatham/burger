e32a890bd061cbe72036ba6c60bd76bf3ba62d70
Calley O'Neil <calleyoneil@Calleys-MacBook-Pro.local>
2017-05-09 21:25:47 -0500

    creating all the files

---
+++ models/burger.js
@@ -0,0 +1,38 @@
+var orm = require("../config/orm.js");
+
+//create object
+var burger = {
+    selectAll: function () {
+        orm.selectAll('burger', function(res) {
+            return new Promise(resolve, reject) {
+                if(res) {
+
+                    resolve(res)
+                } else {
+                    reject()
+                }
+            }
+        })
+    }
+}
+
+module.exports = burger;
+
+burger.selectAll(function(res) {
+    console.log(res)
+})
+
+burger
+    .selectAll()
+    .then(function(res) {
+
+    })
+    .catch(function(err) {
+        
+    })
+
+
+
+
+
+
